27437334f711162d1f0c6826d6406166afb5a9b1
mesche-uni <eschema80762@th-nuernberg.de>
2021-11-07 16:17:03 +0100

    CC-9 complete RandomLines

--- ofMarvinSpielwiese/src/ofRandomLines.cpp
+++ ofMarvinSpielwiese/src/ofRandomLines.cpp
@@ -1,4 +1,11 @@
 #include "ofRandomLines.h"
+/*
+Titel:		 RandomLines
+Description: 1st file of following a tutorial by Daniel Buzzo
+Link:		 https://www.youtube.com/watch?v=2Jz0LE6ovvg&list=PL6QF0yo3Zj7DbN76C5-_6VCDF5CPBIz6l
+*/
+
+
 ofColor myDrawCol;
 int frameRate;
 
@@ -20,7 +27,6 @@ void ofRandomLines::update() {
 
 
 //--------------------------------------------------------------
-
 void ofRandomLines::draw() {
 	if (0) drawRandomPolyLine();
 	if (0) drawRandomLines();
@@ -28,6 +34,7 @@ void ofRandomLines::draw() {
 	if (1) drawPointAtCurvedLine();
 }
 
+//--------------------------------------------------------------
 void ofRandomLines::keyPressed(int key) {
 	//change between Fullscreen or Window		
 	if (key == 'f') ofToggleFullscreen();
@@ -36,22 +43,25 @@ void ofRandomLines::keyPressed(int key) {
 	if (key == 'l') frameRate--;
 }
 
+//--- CurvedPolyLine --------------------------------------------------------------
 void ofRandomLines::drawCurvedPolyLine() {
 	myLine.clear();
 	for (int i = 0; i < 10; i++) myLine.curveTo(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()));
 	myLine.close();
 	linePercent = 0;
 }
+
 void ofRandomLines::drawPointAtCurvedLine() {
 	linePercent += 0.005;
 	ofDrawCircle(myLine.getPointAtPercent(linePercent), 20);
 }
 
-
+//--- RandomPolyLine --------------------------------------------------------------
 void ofRandomLines::drawRandomPolyLine() {
 	myLine.addVertex(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()));
 }
 
+//--- RandomLines --------------------------------------------------------------
 void ofRandomLines::drawRandomLines() {
 
 	for (int i = 0; i < 100; i++)
