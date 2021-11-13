eb0d2e2792e4d25399791c1a4c75ae8c0bfcbd8f
mesche-uni <eschema80762@th-nuernberg.de>
2021-11-13 13:26:18 +0100

    CC-9 reset ofApp

--- ofMarvinSpielwiese/src/ofApp.cpp
+++ ofMarvinSpielwiese/src/ofApp.cpp
@@ -1,87 +1,22 @@
 #include "ofApp.h"
 
-int width = 0;
-ofRectangle myRect;
-int myRectSize = 50;
-
-ofRectangle circleArray[];
-
-int yMovement = 1;
-int xMovement = 1;
-
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofSetWindowTitle("OpenFramworks 'Windowtitel'");
-	myRect.set(ofGetWidth() / 2 - myRectSize / 2, ofGetHeight() / 2 - myRectSize / 2, myRectSize, myRectSize);
-	
+	ofSetWindowTitle("Nervensystem");
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	if (collisionWithWall()) {
-		cout << "bounce" << endl;
-		ofDrawCircle(myRect.getCenter(), 50);
-	}
-}
 
-
-bool ofApp::collisionWithWall() {
-	bool collision = false;
-
-	if (myRect.getTop() <= 0)
-	{
-		myRect.setY(0);
-		yMovement = -yMovement;
-		collision = true;
-	}
-	if (myRect.getBottom() >= ofGetHeight())
-	{
-		myRect.setY(ofGetHeight() - myRectSize);
-		yMovement = -yMovement;
-		collision = true;
-	}
-
-	if (myRect.getLeft() <= 0)
-	{
-		myRect.setX(0);
-		xMovement = -xMovement;
-		collision = true;
-	}
-	if (myRect.getRight() >= ofGetWidth())
-	{
-		myRect.setX(ofGetWidth() - myRectSize);
-		xMovement = -xMovement;
-		collision = true;
-	}
-
-	myRect.setPosition(myRect.getX() + xMovement, myRect.getY() + yMovement);
-	return collision;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	
-	//ofDrawCircle(ofGetWidth() / 2, ofGetHeight() / 2, 50);
-
-	ofDrawLine(myRect.getX()+ myRectSize/2, myRect.getY() + myRectSize / 2,ofGetWidth() / 2, ofGetHeight() / 2);
-
-	ofDrawRectangle(myRect);
-
-
-
-	if (width != int(ofGetWidth()))
-	{
-		width = ofGetWidth();
-		cout << width << endl;
-	}
 
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
-	//change between Fullscreen or Window		
-	if (key == 'f') ofToggleFullscreen();
 
 }
 
--- ofMarvinSpielwiese/src/ofApp.h
+++ ofMarvinSpielwiese/src/ofApp.h
@@ -9,9 +9,6 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		bool collisionWithWall();
-
-
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
