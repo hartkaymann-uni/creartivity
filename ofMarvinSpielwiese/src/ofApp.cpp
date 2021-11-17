09a0a6963eb91ce187850f01c2844bace524f73e
mesche-uni <eschema80762@th-nuernberg.de>
2021-11-17 16:59:41 +0100

    CC-9 2st update of NN via class

--- ofMarvinSpielwiese/src/ofApp.cpp
+++ ofMarvinSpielwiese/src/ofApp.cpp
@@ -1,111 +1,161 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-	//ofSetFrameRate(60);
+void ofApp::setup() {
+	bool pause = false;
+	ofSetFrameRate(30);
+	int maxDistance = 100;
+	bool lineIsEnabled = false;
+	bool lightningIsEnabled = false;
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-	for (int i = 0; i < hubs.size(); i++)
+void ofApp::update() {
+
+	// Opportunity to pause
+	if (isPaused) return;
+
+	// Update or delete existing nodes
+	for (int i = 0; i < nodes.size(); i++)
 	{
-		if (hubs[i].isVisible())
+		if (nodes[i].isVisible())
 		{
-			hubs[i].update();
+			nodes[i].update();
 		}
 		else
 		{
-			hubs.erase(hubs.begin()+i);
+			nodes.erase(nodes.begin() + i);
 		}
 	}
 
-	
-	while (hubs.size() < 128)
+	// Generate some random nodes
+	while (nodes.size() < 128)
 	{
-		Hub newHub(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()), hue);
-		hubs.push_back(newHub);
+		Node newHub(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()));
+		nodes.push_back(newHub);
 	}
-
 }
 
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 	ofSetColor(255);
-	for (int i = 0; i < hubs.size(); i++)
+
+	// Draw circles for every node
+	for (int i = 0; i < nodes.size(); i++)
 	{
-		hubs[i].draw();
+		nodes[i].draw();
+	}
+
+	// Draw lines or lightning between nodes
+	for (int i = 0; i < nodes.size(); i++)
+	{
+		glm::vec2 positionOne = nodes[i].position;
+		for (int j = 0; j < nodes.size(); j++)
+		{
+			glm::vec2 positionTwo = nodes[j].position;
+			int distance = glm::distance(positionOne, positionTwo);
+			if (i != j && distance < maxDistance) // watch for double checking!
+			{
+				if(lineIsEnabled) ofDrawLine(positionOne, positionTwo);
+				if(lightningIsEnabled) drawLightning(positionOne, positionTwo, distance/3);
+			}
+		}
+	}
+
+}
+
+void ofApp::drawLightning(glm::vec2 Pos1, glm::vec2 Pos2, float displace) {
+
+	if (displace <= 1) ofDrawLine(Pos1, Pos2);
+	else
+	{
+		float mid_x = (Pos1.x + Pos2.x) / 2 + (ofRandom(1) - 0.5) * displace;
+		float mid_y = (Pos1.y + Pos2.y) / 2 + (ofRandom(1) - 0.5) * displace;
+
+		mid_x += (ofRandom(1) - 0.5) * displace;
+		mid_y += (ofRandom(1) - 0.5) * displace;
+
+		glm::vec2 mid = glm::vec2(mid_x, mid_y);
+
+		drawLightning(Pos1, mid, displace / 2);
+		drawLightning(Pos2, mid, displace / 2);
 	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
 	if (key == 'f') ofToggleFullscreen();
+	if (key == 'p') isPaused = !isPaused;
+	if (key == 'w') maxDistance += 10;
+	if (key == 's') maxDistance -= 10;
+	if (key == '1') lineIsEnabled = !lineIsEnabled;
+	if (key == '2') lightningIsEnabled = !lightningIsEnabled;
 }
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-	Hub newHub(mouseX, mouseY, hue);
-	hubs.push_back(newHub);
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseDragged(int x, int y, int button) {
+	Node newNode(mouseX, mouseY);
+	nodes.push_back(newNode);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-Hub::Hub(int startX, int startY, int hue) {
-	float directionSpeed = 1;
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
+
+//--------------------------------------------------------------
+Node::Node(int startX, int startY) {
+	float directionSpeed = 1.0;
 	position = glm::vec2(startX, startY);
 	direction = glm::vec2(ofRandom(-directionSpeed, directionSpeed), ofRandom(-directionSpeed, directionSpeed));
 	size = 20;
 }
 
-Hub::~Hub() {
+Node::~Node() {
 
 }
 
-bool Hub::isVisible() {
+bool Node::isVisible() {
 	int overlap = 20;
 	return(position.x > -overlap &&
 		position.x < ofGetWidth() + overlap &&
@@ -114,12 +164,13 @@ bool Hub::isVisible() {
 		size > 1);
 }
 
-void Hub::update() {
+void Node::update() {
 	float time = ofGetElapsedTimef();
 	position += direction;
 	size -= 0.01;
 }
 
-void Hub::draw() {
-	ofDrawCircle(position,size);
+void Node::draw() {
+	ofNoFill();
+	ofDrawCircle(position, size);
 }
--- ofMarvinSpielwiese/src/ofApp.h
+++ ofMarvinSpielwiese/src/ofApp.h
@@ -2,7 +2,7 @@
 
 #include "ofMain.h"
 
-class Hub {
+class Node {
 
 public:
 	glm::vec2 force;
@@ -16,33 +16,40 @@ public:
 	void update();
 	void draw();
 
-	Hub(int x, int y, int hue);		//construktor
-	~Hub();							//destruktor
+	Node(int x, int y);		//construktor
+	~Node();							//destruktor
 
 };
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+	void drawLightning(glm::vec2 Pos1, glm::vec2 Pos2, float displace = 50);
 
-		vector<Hub> hubs;
-		int hue;
-	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+private:
+
+	vector<Node> nodes;
+	bool isPaused;
+	bool lineIsEnabled;
+	bool lightningIsEnabled;
+	int maxDistance;
+
 };
 
 
