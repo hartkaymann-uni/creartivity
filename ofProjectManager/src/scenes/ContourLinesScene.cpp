f75d605630883f774d9dca40f5e169cdfbf04a73
mesche-uni <eschema80762@th-nuernberg.de>
2021-12-03 12:47:33 +0100

    CC-34 initialisation of ContourLinesScene + add new files

---
+++ ofProjectManager/src/scenes/ContourLinesScene.cpp
@@ -0,0 +1,61 @@
+#include "ContourLinesScene.h"
+
+ContourLinesScene::ContourLinesScene():ofxScene("ContourLines")
+{
+}
+
+void ContourLinesScene::setup()
+{
+}
+
+void ContourLinesScene::update()
+{
+}
+
+void ContourLinesScene::draw()
+{
+}
+
+void ContourLinesScene::keyPressed(int key)
+{
+}
+
+void ContourLinesScene::keyReleased(int key)
+{
+}
+
+void ContourLinesScene::mouseMoved(int x, int y)
+{
+}
+
+void ContourLinesScene::mouseDragged(int x, int y, int button)
+{
+}
+
+void ContourLinesScene::mousePressed(int x, int y, int button)
+{
+}
+
+void ContourLinesScene::mouseReleased(int x, int y, int button)
+{
+}
+
+void ContourLinesScene::mouseEntered(int x, int y)
+{
+}
+
+void ContourLinesScene::mouseExited(int x, int y)
+{
+}
+
+void ContourLinesScene::windowResized(int w, int h)
+{
+}
+
+void ContourLinesScene::dragEvent(ofDragInfo dragInfo)
+{
+}
+
+void ContourLinesScene::gotMessage(ofMessage msg)
+{
+}
