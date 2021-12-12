d92301e18f773771d5c57d1ee33a97adab249145
Cosmo Strattner <cosmo@symbioware.local>
2021-12-12 16:35:52 +0100

    Erster Checkin

---
+++ ofCosmosSpielwiese/res/shaders/particle.frag
@@ -0,0 +1,12 @@
+#version 430
+
+//precision mediump float;
+
+//uniform vec4 color;
+
+out vec4 outputColor;
+
+void main() {
+    //gl_FragColor = color;
+    outputColor = vec4(0.09, 0.45, 0.15, 1);
+}
--- ofCosmosSpielwiese/src/ofApp.h
+++ ofCosmosSpielwiese/src/ofApp.h
@@ -1,24 +1,50 @@
 #pragma once
 
 #include "ofMain.h"
+#include "particles/particle.h"
+#include "helper.h"
+#include "particles/ParticleSystem.h"
+#include "ofxGui.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	void keyPressed(int key);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+
+private:
+	// Shader
+	ofShader particleShader;
+	ofShader particleTrailShader;
+
+
+	// Input
+	int mouseX;
+	int mouseY;
+	int mouseXClick;
+	int mouseYClick;
+	bool isPressingMouse;
+ 
+	void updateMousePos(int x, int y, string action = "default");
+
+	// Particles
+	void setUpParticles();
+	void updateParticles();
+	void drawParticles();
+	vector<particle> particles;
+	int hue;
+	ParticleSystem particleSystem;
+
+	// Abilities
+	bool isChargingImpulse;
+
+	// GUI
+	ofxIntSlider trail;
+	ofxPanel gui;
 };
