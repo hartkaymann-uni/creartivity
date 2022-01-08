678e390131d45b9a3de021cf340e29fa8b990a73
Kay Hartmann <kay-hartmann@gmx.net>
2022-01-08 14:39:51 +0100

    CC-35 dont show cuboid scene

--- ofProjectManager/src/ofApp.cpp
+++ ofProjectManager/src/ofApp.cpp
@@ -12,10 +12,10 @@ void ofApp::setup() {
 	setTransformer( &transformer );
 
 	// Load scenes
-	//scenes.push_back( (ParticleScene*)sceneManager.add( new ParticleScene() ) );
+	// scenes.push_back( (ParticleScene*)sceneManager.add( new ParticleScene() ) );
+	// scenes.push_back( (CuboidScene*)sceneManager.add( new CuboidScene() ) );
 	scenes.push_back( (SpiralScene*)sceneManager.add( new SpiralScene() ) );
 	scenes.push_back( (GameOfLifeScene*)sceneManager.add( new GameOfLifeScene() ) );
-	scenes.push_back( (CuboidScene*)sceneManager.add( new CuboidScene() ) );
 	scenes.push_back( (SwarmScene*)sceneManager.add( new SwarmScene() ) );
 	scenes.push_back( (ContourLinesScene*)sceneManager.add( new ContourLinesScene() ) );
 	sceneManager.setup( true ); // Setup all scenes now
