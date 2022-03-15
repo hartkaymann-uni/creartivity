94d131161f1e05debb19d257c13c0d0687f64c49
Kay Hartmann <kay-hartmann@gmx.net>
2022-03-15 23:47:39 +0100

    CC-44

--- ofProjectManager/src/scenes/fluid/FluidScene.cpp
+++ ofProjectManager/src/scenes/fluid/FluidScene.cpp
@@ -11,13 +11,24 @@ void FluidScene::setup()
 
 void FluidScene::update()
 {
-	for (ofPoint const& p : particles) {
+	for (ofPoint& p : particles) {
+		if (p.x > ofGetWidth()) p.x = 0;
+		if (p.y > ofGetHeight()) p.y = 0;
+		if (p.z > ofGetHeight()) p.z = 0;
+
+		
 	}
 }
 
 void FluidScene::draw()
 {
 	camera.begin();
+
+	float width = ofGetWidth();
+	float height = ofGetHeight();
+	float depth = height;
+
+	ofDrawBox( width * 0.5f, height * 0.5f, depth * 0.5f, width, height, depth );
 
 	for (ofPoint const& p : particles) {
 		ofDrawCircle( p, 3 );
