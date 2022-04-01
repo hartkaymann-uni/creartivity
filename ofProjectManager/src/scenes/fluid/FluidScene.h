cea83d1effe94079ed33ef7a6f0b436aa44ccfab
hartkaymann <hartmannka80488@th-nuernberg.de>
2022-04-01 13:02:06 +0200

    CC-66 created solver class

--- ofProjectManager/src/scenes/fluid/FluidScene.h
+++ ofProjectManager/src/scenes/fluid/FluidScene.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "ccFluidSolver.h"
+
 /*
 	Fluid Scene
 	Simulation of an incompressible, homogenous fluid using the navier-stokes equaution.
@@ -21,28 +23,9 @@ public:
 	void keyReleased( int key );
 
 private:
-	float time = 0.f;
-	float dt = 0.f;
-	float timestep = 1.f;
+
 	bool debug = false;
 	bool step = false;
-	float splatRadius = 0.1;
-
-	struct Grid {
-		glm::vec2 size;
-		float scale;
-		bool applyBounds;
-	};
-	Grid grid = { {512 * 2, 256*2}, 1.f, true };
-	ofPlanePrimitive plane;
-
-	bool applyVorticity = true;
-	bool applyViscosity = false;
-	float viscosity = 0.3;
-	float epsilon = 0.00024414;
-	float curl = 0.3;
-	int jacobiIterations = 20; // normal: 20-50
-	float dissipation = 0.998; // none:1, slow:0.998, fast=0.992, veryfast=0.9
 
 	ofParameterGroup groupGeneral;
 	ofParameterGroup groupView;
@@ -63,41 +46,8 @@ private:
 	ofParameter<int> p_JacobiIterations;
 	ofParameter<float> p_Dissipation;
 
-	// Vector Field
-	ofFbo velocity;
-	// Scalar Fields
-	ofFbo density;
-	ofFbo velocityDivergence;
-	ofFbo velocityVorticity;
-	ofFbo pressure;
-
-	ofShader advectProgram;
-	ofShader gradientProgram;
-	ofShader jacobiscalarProgram;
-	ofShader jacobivectorProgram;
-	ofShader divergenceProgram;
-	ofShader vorticityProgram;
-	ofShader vorticityforceProgram;
-	ofShader boundariesProgram;
-	ofShader splatProgram;
+	ccFluidSolver solver;
 
 	ofShader displayVectorProgram;
-
-	void project();
-	void addForces();
-
-	// Slabops
-	void advect( ofFbo& advected, ofFbo& output, float d );
-	void boundary( ofFbo& input, ofFbo& output, float scale = -1.f );
-	void boundarySide( ofFbo& input, ofFbo& output, ofPolyline& line, glm::vec2 offset, float scale );
-	void vortex( ofFbo& output );
-	void vortexConfine( ofFbo& vorticity, ofFbo& output );
-	void diffuse( ofShader& jacobi, ofFbo& x, ofFbo& b, ofFbo& output, float alpha = -1.f, float beta = 4.f, float scale = -1.f );
-	void diffuseStep( ofShader& jacobi, ofFbo& x, ofFbo& b, ofFbo& output, float alpha, float beta );
-	void diverge( ofFbo& divergence );
-	void gradiate( ofFbo& output );
-	void splat( ofFbo& read, glm::vec3 color, glm::vec2 point );
-
-	void clearBuffer( ofFbo& buffer );
 
 };
