0e729f035ae776febef70abe1b99813db4c980b9
Kay Hartmann <kay-hartmann@gmx.net>
2022-04-16 23:13:25 +0200

    CC-80 updated ofInput

--- ofInput/src/ofApp.h
+++ ofInput/src/ofApp.h
@@ -2,15 +2,17 @@
 
 #include "ofMain.h"
 #include "ofxOsc.h"
+#include "ofxGui.h"
 
 #define HOST "localhost"
 #define PORT 12345
-#define NO_USERS 10
 
 class ofApp : public ofBaseApp {
 
 	struct user {
-		ofVec2f position;
+		int id;
+		glm::vec2 left;
+		glm::vec2 right;
 	};
 
 public:
@@ -18,24 +20,31 @@ public:
 	void update();
 	void draw();
 
-	void keyPressed( int key );
-	void keyReleased( int key );
-	void mouseMoved( int x, int y );
 	void mouseDragged( int x, int y, int button );
 	void mousePressed( int x, int y, int button );
 	void mouseReleased( int x, int y, int button );
-	void mouseEntered( int x, int y );
-	void mouseExited( int x, int y );
+
 	void windowResized( int w, int h );
-	void dragEvent( ofDragInfo dragInfo );
-	void gotMessage( ofMessage msg );
 
 	int width, height;
-
-	ofxOscSender sender;
+	ofxPanel gui;
+	ofParameter<bool> p_add;
+	ofParameter<bool> p_remove;
 
 	std::map<int, user> users;
 
-	void sendUser(int id, user& user);
+	ofxOscSender sender;
 
+	glm::vec2 mapped(glm::vec2 v);
+	glm::vec2 unmapped(glm::vec2 v);
+
+	void sendUser( int id, user& user );
+	void registerUser( int id );
+	void removeUser( int id );
+
+	void printUsers();
+	void sendConnectionStarted();
+
+	void handleRegisterUser( bool& b );
+	void handleRemoveUser( bool& b );
 };
